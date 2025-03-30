1bb3fa280eee4229f3339eba5e02b2b6499f4191
sajid-edninja <ssk73573@gmail.com>
2025-03-30 16:01:32 +0530

    add POST req

--- app/api/v1/user/details/route.ts
+++ app/api/v1/user/details/route.ts
@@ -6,3 +6,10 @@ export function GET() {
     email: "sajid@gmail.com",
   });
 }
+
+export function POST() {
+  return NextResponse.json({
+    user: "Sajid",
+    email: "sajid@gmail.com",
+  });
+}
