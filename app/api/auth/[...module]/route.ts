5069c5a3c32971f58e0d37d47c12a41ac65eb8e1
sajid-edninja <ssk73573@gmail.com>
2025-04-03 09:55:17 +0530

    add next auth credentials based login

--- app/api/auth/[...module]/route.ts
+++ app/api/auth/[...module]/route.ts
@@ -1,5 +1,30 @@
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-const handler = NextAuth();
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "sajid" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const username = credentials?.username;
+        const password = credentials?.password;
 
-export { handler as GET, handler as POST };
+        // db request
+        const user = {
+          name: "sajid",
+          id: 1,
+          username: "sajid@gmail.com",
+        };
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+});
